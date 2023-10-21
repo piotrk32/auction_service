edf30d2ab014e3d85e9c5067bdf298348058dd62
piotrk32 <piotrkepistyuwm@gmail.com>
2023-10-21 13:50:04 +0200

    basic entity added

--- src/main/java/com/example/auction_service/models/BasicEntity.java
+++ src/main/java/com/example/auction_service/models/BasicEntity.java
@@ -1,5 +1,11 @@
+package com.example.auction_service.models;
+
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
