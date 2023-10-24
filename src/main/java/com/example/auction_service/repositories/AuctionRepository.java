4b339542ab012e1ac22397c19f5f57749836ec15
piotrk32 <piotrkepistyuwm@gmail.com>
2023-10-24 15:34:02 +0200

    AuctionRepository added, Paging added

---
+++ src/main/java/com/example/auction_service/repositories/AuctionRepository.java
@@ -0,0 +1,23 @@
+package com.example.auction_service.repositories;
+
+import com.example.auction_service.models.auction.Auction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface AuctionRepository extends JpaRepository<Auction, Long>, JpaSpecificationExecutor<Auction> {
+
+    @Query("""
+            SELECT o.provider.email
+            FROM Auction o
+            WHERE o.id = :offeringId
+            """)
+    Optional<String> getProviderEmailByAuctionId(@Param("offeringId") Long offeringId);
+
+    Page<Auction> findAllByProviderIdAndIsActiveTrue(Long providerId, PageRequest pageRequest);
+}
