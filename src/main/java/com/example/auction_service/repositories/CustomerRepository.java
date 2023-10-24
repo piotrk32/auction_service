e6309ca9e7869a31cc6e55d7547528517acf276d
piotrk32 <piotrkepistyuwm@gmail.com>
2023-10-24 15:29:20 +0200

    CustomerRepository added

---
+++ src/main/java/com/example/auction_service/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.auction_service.repositories;
+
+import com.example.auction_service.models.customer.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+
+}
