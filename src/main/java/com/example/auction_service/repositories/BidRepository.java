e28d8d311bccf8e8bbb10a26bd7765eb34892708
piotrk32 <piotrkepistyuwm@gmail.com>
2023-10-24 15:53:28 +0200

    BidRepository added

---
+++ src/main/java/com/example/auction_service/repositories/BidRepository.java
@@ -0,0 +1,23 @@
+package com.example.auction_service.repositories;
+
+import com.example.auction_service.models.bid.Bid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface BidRepository extends JpaRepository<Bid, Long>, JpaSpecificationExecutor<Bid> {
+    Page<Bid> findAllByAuctionId(Long auctionId, PageRequest pageRequest);
+
+    @Query("""
+            SELECT b.customer.email
+            FROM Bid b
+            WHERE b.id = :bidId
+            """)
+    Optional<String> getCustomerEmailByBidId(@Param("bidId") Long bidId);
+
+}
