f690cc718a22254be8b57fd0c70c9df4281b6d52
piotrk32 <piotrkepistyuwm@gmail.com>
2023-10-24 16:17:58 +0200

    ItemRepository added, searching methods added

---
+++ src/main/java/com/example/auction_service/repositories/ItemRepository.java
@@ -0,0 +1,26 @@
+package com.example.auction_service.repositories;
+
+import com.example.auction_service.models.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
+
+    List<Item> findAllByProviderId(Long providerId);
+
+    List<Item> findAllByAuctionId(Long auctionId);
+    @Query("""
+            SELECT i.provider.email
+            FROM Item i
+            WHERE i.id = :itemId
+            """)
+    String getProviderEmailByItemId(@Param("itemId") Long itemId);
+
+    List<Item> findByItemNameContainingIgnoreCase(String nameFragment);
+
+    List<Item> findByItemNameContainingIgnoreCaseAndAuction_IsActiveTrue(String nameFragment);
+}
