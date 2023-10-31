ae1f6263d5dcc33a714d5e176d89de5b61a8f9d8
piotrk32 <piotrkepistyuwm@gmail.com>
2023-10-31 18:15:37 +0100

    AanmeValidators added

--- src/main/java/com/example/auction_service/models/address/dtos/AddressUpdateDTO.java
+++ src/main/java/com/example/auction_service/models/address/dtos/AddressUpdateDTO.java
@@ -1,4 +1,27 @@
 package com.example.auction_service.models.address.dtos;
 
-public class AddressUpdateDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+public record AddressUpdateDTO(@NotBlank(message = "Country name field must be filled in.")
+                               @Length(min = 4, max = 255, message = "Country name must be between 4 and 255 characters.")
+                               @Pattern(regexp = NAME_REGEX, message = "Country name must contain only letters.")
+                               String country,
+                               @NotBlank(message = "City name field must be filled in.")
+                               @Length(min = 1, max = 255, message = "City name must be between 1 and 255 characters.")
+                               @Pattern(regexp = NAME_REGEX, message = "City name must contain only letters.")
+                               String city,
+                               @NotBlank(message = "Street field must be filled in.")
+                               @Length(min = 1, max = 255, message = "Street must be between 1 and 255 characters.")
+                               String street,
+                               @NotBlank(message = "Zip code field must be filled in.")
+                               @Length(min = 1, max = 10, message = "Zip code must be between 1 and 10 characters.")
+                               String zipCode,
+                               @NotBlank(message = "Building number field must be filled in.")
+                               @Length(min = 1, max = 10, message = "Building number must be between 1 and 10 characters.")
+                               String buildingNumber,
+                               @NotBlank(message = "Building number field must be filled in.")
+                               @Length(min = 1, max = 10, message = "Building number must be between 1 and 10 characters.")
+                               String apartmentNumber) {
 }
---
+++ src/main/java/com/example/auction_service/validators/NameValidators.java
@@ -0,0 +1,16 @@
+package com.example.auction_service.validators;
+
+public class NameValidators {
+
+    public static final String NAME_REGEX = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*$";
+
+    public static boolean containsLettersAndSpaces(String value) {
+        return value.chars()
+                .allMatch(character -> Character.isLetter(character) || Character.isSpaceChar(character));
+    }
+
+    public static boolean containsLettersDigitsAndSpaces(String value) {
+        return value.chars()
+                .allMatch(character -> Character.isLetterOrDigit(character) || Character.isSpaceChar(character));
+    }
+}
