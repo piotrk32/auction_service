5d2014b2e4173b7ef618c3a55e07df13a7772a47
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-08 13:04:09 +0100

    AuctionSpecification added

---
+++ src/main/java/com/example/auction_service/services/auction/AuctionSpecification.java
@@ -0,0 +1,34 @@
+package com.example.auction_service.services.auction;
+
+import com.example.auction_service.models.auction.Auction;
+import com.example.auction_service.models.provider.Provider;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+
+public class AuctionSpecification {
+
+    public static Specification<Auction> auctionNameContains(String auctionName) {
+        return (root, query, cb) -> auctionName == null ? null :
+                cb.like(cb.lower(root.get("auctionName")), "%" + auctionName.toLowerCase() + "%");
+    }
+
+    public static Specification<Auction> priceGreaterThanOrEqual(String price) {
+        return (root, query, cb) -> price == null ? null : cb.greaterThanOrEqualTo(root.get("price"),
+                new BigDecimal(price));
+    }
+
+    public static Specification<Auction> priceLessThanOrEqual(String price) {
+        return (root, query, cb) -> price == null ? null : cb.lessThanOrEqualTo(root.get("price"),
+                new BigDecimal(price));
+    }
+
+    public static Specification<Auction> providerIdEquals(String providerId) {
+        return (root, query, cb) -> {
+            if (providerId == null) return null;
+            Join<Auction, Provider> providerJoin = root.join("provider");
+            return cb.equal(providerJoin.get("id"), Long.valueOf(providerId));
+        };
+    }
+}
