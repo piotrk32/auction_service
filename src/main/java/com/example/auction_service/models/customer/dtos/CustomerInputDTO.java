7350bedfcb105b65d539b33eb292693bdbd068c0
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-11 14:09:35 +0100

    CustomerInputDTO created

---
+++ src/main/java/com/example/auction_service/models/customer/dtos/CustomerInputDTO.java
@@ -0,0 +1,7 @@
+package com.example.auction_service.models.customer.dtos;
+
+import com.example.auction_service.models.user.dtos.UserInputDTO;
+
+public class CustomerInputDTO extends UserInputDTO {
+
+}
--- src/main/java/com/example/auction_service/services/customer/CustomerService.java
+++ src/main/java/com/example/auction_service/services/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package com.example.auction_service.services.customer;
 
 import com.example.auction_service.exceptions.EntityNotFoundException;
+import com.example.auction_service.models.address.Address;
 import com.example.auction_service.models.customer.Customer;
 import com.example.auction_service.repositories.CustomerRepository;
 import com.example.auction_service.repositories.UserRepository;
@@ -18,6 +19,23 @@ public class CustomerService {
         return customerRepository.findById(customerId)
                 .orElseThrow(() -> new EntityNotFoundException("Customer", "No customer found with id: " + customerId));
     }
+
+    public Customer createCustomer(Address address, CustomerInputDTO customerInputDTO, User user) {
+        Customer customer = new Customer(
+                address,
+                customerInputDTO.getFirstName(),
+                customerInputDTO.getLastName(),
+                customerInputDTO.getBirthDate(),
+                user.getEmail(),
+                customerInputDTO.getPhoneNumber(),
+                user.getAccessToken(),
+                user.getRefreshToken(),
+                user.getIdToken());
+        userRepository.delete(user);
+        return customerRepository.saveAndFlush(customer);
+    }
+
+
 
 
 
