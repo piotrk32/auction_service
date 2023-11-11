5d1e4f12ee6bf2726277b26c3f7042b19ed91e91
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-11 20:57:02 +0100

    getCustomersByProviderId facade method added

--- src/main/java/com/example/auction_service/services/provider/ProviderFacade.java
+++ src/main/java/com/example/auction_service/services/provider/ProviderFacade.java
@@ -2,6 +2,9 @@ package com.example.auction_service.services.provider;
 
 import com.example.auction_service.exceptions.user.UserAlreadyExistsException;
 import com.example.auction_service.models.address.Address;
+import com.example.auction_service.models.customer.dtos.CustomerMapper;
+import com.example.auction_service.models.customer.dtos.CustomerRequestDTO;
+import com.example.auction_service.models.customer.dtos.CustomerResponseDTO;
 import com.example.auction_service.models.provider.Provider;
 import com.example.auction_service.models.provider.dtos.ProviderInputDTO;
 import com.example.auction_service.models.provider.dtos.ProviderResponseDTO;
@@ -11,6 +14,7 @@ import com.example.auction_service.services.address.AddressService;
 import com.example.auction_service.services.customer.CustomerService;
 import com.example.auction_service.services.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import static com.example.auction_service.models.provider.dtos.ProviderMapper.mapToProviderResponseDTO;
@@ -55,9 +59,9 @@ public class ProviderFacade {
 
     //TODO CustomerService and Facade
 
-//    public Page<CustomerResponseDTO> getCustomersByProviderId(Long providerId, CustomerRequestDTO customerRequestDTO) {
-//        return customerService.getAllCustomersIdByProvider(providerId, customerRequestDTO).map(CustomerMapper::mapToCustomerResponseDTO);
-//    }
+    public Page<CustomerResponseDTO> getCustomersByProviderId(Long providerId, CustomerRequestDTO customerRequestDTO) {
+        return customerService.getAllCustomersIdByProvider(providerId, customerRequestDTO).map(CustomerMapper::mapToCustomerResponseDTO);
+    }
 
 
 
