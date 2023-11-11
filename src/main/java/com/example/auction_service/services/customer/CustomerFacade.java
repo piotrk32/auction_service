d002f5fe7d5e6f44553a8c9719fd0e477977b23c
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-11 14:28:11 +0100

    CustomerFacade created

--- src/main/java/com/example/auction_service/services/customer/CustomerFacade.java
+++ src/main/java/com/example/auction_service/services/customer/CustomerFacade.java
@@ -1,4 +1,47 @@
 package com.example.auction_service.services.customer;
 
+import com.example.auction_service.exceptions.user.UserAlreadyExistsException;
+import com.example.auction_service.models.address.Address;
+import com.example.auction_service.models.customer.Customer;
+import com.example.auction_service.models.customer.dtos.CustomerInputDTO;
+import com.example.auction_service.models.customer.dtos.CustomerResponseDTO;
+import com.example.auction_service.models.user.User;
+import com.example.auction_service.models.user.enums.Status;
+import com.example.auction_service.services.address.AddressService;
+import com.example.auction_service.services.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static com.example.auction_service.models.customer.dtos.CustomerMapper.mapToCustomerResponseDTO;
+
+@Component
+@RequiredArgsConstructor
 public class CustomerFacade {
+
+    private final CustomerService customerService;
+    private final UserService userService;
+    private final AddressService addressService;
+
+    public CustomerResponseDTO createCustomer(CustomerInputDTO customerInputDTO, String email) {
+        User user = userService.getUserByEmail(email);
+        if (user.getStatus() == Status.REGISTRATION_INCOMPLETE) {
+            Address emptyAddress = addressService.createEmptyAddress();
+            return mapToCustomerResponseDTO(customerService.createCustomer(emptyAddress, customerInputDTO, user));
+        }
+        throw new UserAlreadyExistsException("User", "User already registered.");
+    }
+
+    public void deleteCustomerById(Long customerId) {
+        customerService.deleteCustomerById(customerId);
+    }
+
+    public CustomerResponseDTO updateCustomerById(Long id, CustomerInputDTO customerInputDTO) {
+        Customer customer = customerService.updateCustomerById(id, customerInputDTO);
+        return mapToCustomerResponseDTO(customer);
+    }
+
+    public CustomerResponseDTO getCustomerById(Long customerId) {
+        return mapToCustomerResponseDTO(customerService.getCustomerById(customerId));
+    }
+
 }
