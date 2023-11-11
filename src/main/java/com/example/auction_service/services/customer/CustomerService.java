13bf1be4bedbf6016221821889a11b06acd6c4c3
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-11 14:25:26 +0100

    getAllCustomersIdByProvider created

--- src/main/java/com/example/auction_service/services/customer/CustomerService.java
+++ src/main/java/com/example/auction_service/services/customer/CustomerService.java
@@ -4,11 +4,16 @@ import com.example.auction_service.exceptions.EntityNotFoundException;
 import com.example.auction_service.models.address.Address;
 import com.example.auction_service.models.customer.Customer;
 import com.example.auction_service.models.customer.dtos.CustomerInputDTO;
+import com.example.auction_service.models.customer.dtos.CustomerRequestDTO;
 import com.example.auction_service.models.user.User;
 import com.example.auction_service.models.user.enums.Status;
 import com.example.auction_service.repositories.CustomerRepository;
 import com.example.auction_service.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -52,6 +57,40 @@ public class CustomerService {
         customer.setPhoneNumber(customerInputDTO.getPhoneNumber());
 
         return customerRepository.saveAndFlush(customer);
+    }
+
+    public Page<Customer> getAllCustomersIdByProvider(Long providerId, CustomerRequestDTO customerRequestDTO) {
+        PageRequest pageRequest = PageRequest.of(
+                Integer.parseInt(customerRequestDTO.getPage()),
+                Integer.parseInt(customerRequestDTO.getSize()),
+                Sort.Direction.valueOf(customerRequestDTO.getDirection()),
+                customerRequestDTO.getSortParam());
+
+        Specification<Customer> spec = Specification.where(null);
+
+        if (customerRequestDTO.getFirstName() != null) {
+            spec = spec.and(CustomerSpecification.firstNameContains(customerRequestDTO.getFirstName()));
+        }
+        if (customerRequestDTO.getLastName() != null) {
+            spec = spec.and(CustomerSpecification.lastNameContains(customerRequestDTO.getLastName()));
+        }
+        if (customerRequestDTO.getEmail() != null) {
+            spec = spec.and(CustomerSpecification.emailContains(customerRequestDTO.getEmail()));
+        }
+        if (customerRequestDTO.getPhoneNumber() != null) {
+            spec = spec.and(CustomerSpecification.phoneNumberContains(customerRequestDTO.getPhoneNumber()));
+        }
+        if (customerRequestDTO.getBirthDate() != null) {
+            spec = spec.and(CustomerSpecification.birthDateEquals(customerRequestDTO.getBirthDate()));
+        }
+        if (customerRequestDTO.getStatus() != null) {
+            spec = spec.and(CustomerSpecification.statusEquals(customerRequestDTO.getStatus()));
+        }
+        if (providerId != null) {
+            spec = spec.and(CustomerSpecification.customersByProviderId(providerId));
+        }
+
+        return customerRepository.findAll(spec, pageRequest);
     }
 
 
