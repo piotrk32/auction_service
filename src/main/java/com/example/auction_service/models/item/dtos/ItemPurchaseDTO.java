b8fbd2126ff7b65522441e24d5032cc04403a3c5
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-18 19:58:22 +0100

    ItemService updated - buyNow method added, ItemPurchaseDTO created

---
+++ src/main/java/com/example/auction_service/models/item/dtos/ItemPurchaseDTO.java
@@ -0,0 +1,15 @@
+package com.example.auction_service.models.item.dtos;
+
+import lombok.Builder;
+
+public record ItemPurchaseDTO(
+        Long itemId,
+        Long providerId,
+        String itemName,
+        Double buyNowPrice,
+        Boolean isSold) {
+
+    @Builder
+    public ItemPurchaseDTO {}
+
+}
--- src/main/java/com/example/auction_service/services/item/ItemService.java
+++ src/main/java/com/example/auction_service/services/item/ItemService.java
@@ -3,6 +3,7 @@ package com.example.auction_service.services.item;
 import com.example.auction_service.exceptions.EntityNotFoundException;
 import com.example.auction_service.models.item.Item;
 import com.example.auction_service.models.item.dtos.ItemInputDTO;
+import com.example.auction_service.models.item.dtos.ItemPurchaseDTO;
 import com.example.auction_service.models.provider.Provider;
 import com.example.auction_service.repositories.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -27,7 +28,8 @@ public class ItemService {
                 itemInputDTO.startingPrice(),
                 itemInputDTO.imageUrl(),
                 itemInputDTO.isBuyNowActive(),
-                itemInputDTO.buyNowPrice()
+                itemInputDTO.buyNowPrice(),
+                itemInputDTO.isSold()
         );
 
         return itemRepository.saveAndFlush(item);
@@ -48,5 +50,29 @@ public class ItemService {
     public void deleteItemById(Long itemId) {
         Item item = getItemById(itemId);
         itemRepository.delete(item);
+    }
+
+    public ItemPurchaseDTO buyNow(Long itemId, Long customerId) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new EntityNotFoundException("Item", "No item found with id: " + itemId));
+
+        if (!item.getIsBuyNowActive()) {
+            throw new IllegalStateException("Buy now option is not active for this item.");
+        }
+
+        if (item.getIsSold()) {
+            throw new IllegalStateException("Item has been sold.");
+        }
+
+        item.setIsSold(true); // Ustawienie stanu przedmiotu na sprzedany
+        itemRepository.save(item);
+
+        return ItemPurchaseDTO.builder()
+                .itemId(item.getId())
+                .providerId(item.getProvider().getId())
+                .itemName(item.getItemName())
+                .buyNowPrice(item.getBuyNowPrice())
+                .isSold(item.getIsSold())
+                .build();
     }
 }
