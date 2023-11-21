da622a18789d8db28e5944a2c4d3ec2649541897
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-21 14:02:07 +0100

    Filtration getItems added

--- src/main/java/com/example/auction_service/services/item/ItemService.java
+++ src/main/java/com/example/auction_service/services/item/ItemService.java
@@ -4,9 +4,14 @@ import com.example.auction_service.exceptions.EntityNotFoundException;
 import com.example.auction_service.models.item.Item;
 import com.example.auction_service.models.item.dtos.ItemInputDTO;
 import com.example.auction_service.models.item.dtos.ItemPurchaseDTO;
+import com.example.auction_service.models.item.dtos.ItemRequestDTO;
 import com.example.auction_service.models.provider.Provider;
 import com.example.auction_service.repositories.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -75,4 +80,35 @@ public class ItemService {
                 .isSold(item.getIsSold())
                 .build();
     }
+    public Page<Item> getItems(ItemRequestDTO itemRequestDTO) {
+        PageRequest pageRequest = PageRequest.of(
+                Integer.parseInt(itemRequestDTO.getPage()),
+                Integer.parseInt(itemRequestDTO.getSize()),
+                Sort.Direction.fromString(itemRequestDTO.getDirection()),
+                itemRequestDTO.getSortParam());
+
+        Specification<Item> spec = Specification.where(null);
+
+        if (itemRequestDTO.getItemNameSearch() != null) {
+            spec = spec.and(ItemSpecification.itemNameContains(itemRequestDTO.getItemNameSearch()));
+        }
+        if (itemRequestDTO.getItemCategorySearch() != null) {
+            spec = spec.and(ItemSpecification.itemCategoryContains(itemRequestDTO.getItemCategorySearch()));
+        }
+        if (itemRequestDTO.getPriceFrom() != null) {
+            spec = spec.and(ItemSpecification.priceGreaterThanOrEqual(Double.parseDouble(itemRequestDTO.getPriceFrom())));
+        }
+        if (itemRequestDTO.getPriceTo() != null) {
+            spec = spec.and(ItemSpecification.priceLessThanOrEqual(Double.parseDouble(itemRequestDTO.getPriceTo())));
+        }
+        if (itemRequestDTO.getIsBuyNowActive() != null) {
+            spec = spec.and(ItemSpecification.isBuyNowActive(itemRequestDTO.getIsBuyNowActive()));
+        }
+        if (itemRequestDTO.getIsSold() != null) {
+            spec = spec.and(ItemSpecification.isSold(itemRequestDTO.getIsSold()));
+        }
+
+        return itemRepository.findAll(spec, pageRequest);
+    }
 }
+
