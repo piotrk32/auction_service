f283c1f463774f46af5b4792f0039f155d80d0f5
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-22 19:12:36 +0100

    flyway script added

---
+++ src/main/resources/db/migration/V13__DeleteArchivingColumns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.providers DROP COLUMN created_at;
+ALTER TABLE public.providers DROP COLUMN modified_at;
+
+ALTER TABLE public.customers DROP COLUMN created_at;
+ALTER TABLE public.customers DROP COLUMN modified_at;
