ed37485dc29840cfee4110eafa35d7994285d635
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-22 17:52:14 +0100

    annotation added to enable jpa auditing

--- src/main/java/com/example/auction_service/AuctionServiceApplication.java
+++ src/main/java/com/example/auction_service/AuctionServiceApplication.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @OpenAPIDefinition(info = @Info(title = "Apply Default Global SecurityScheme in springdoc-openapi", version = "1.0.0"), security = { @SecurityRequirement(name = "api_key") })
 public class AuctionServiceApplication {
 
