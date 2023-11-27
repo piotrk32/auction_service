ca797de3f51aa6034bd9e6bb9e24a6380c3b9f81
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-28 00:49:47 +0100

    AuctionInputDTO unecessary fields removed

--- src/main/java/com/example/auction_service/models/auction/dtos/AuctionInputDTO.java
+++ src/main/java/com/example/auction_service/models/auction/dtos/AuctionInputDTO.java
@@ -1,6 +1,7 @@
 package com.example.auction_service.models.auction.dtos;
 
 import com.example.auction_service.models.auction.enums.Currency;
+import com.example.auction_service.validators.item.ItemValidationGroups;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,9 +10,6 @@ import org.hibernate.validator.constraints.Length;
 import java.time.LocalDateTime;
 
 public record AuctionInputDTO(
-        @NotNull(message = "Provider id cannot be empty.")
-        Long providerId,
-
         @NotBlank(message = "Auction name field must be filled in.")
         @Length(min = 3, message = "Auction name must be at least 3 characters.")
         String auctionName,
@@ -19,32 +17,27 @@ public record AuctionInputDTO(
         @NotBlank(message = "Auction description field must be filled in.")
         String description,
 
-        @NotNull(message = "Duration cannot be empty.")
-        @Min(value = 0, message = "Duration must not be less than or equal to zero.")
-        Integer duration,
-
-        @NotNull(message = "Price cannot be empty.")
-        @Min(value = 0, message = "Price must not be less than or equal to zero.")
-        Double price,
+//        Integer duration,
 
         @NotNull(message = "Currency cannot be empty.")
         Currency currency,
+
         @NotNull(message = "Category cannot be empty.")
         String category,
 
         @NotNull(message = "Auction start date cannot be empty.")
         LocalDateTime auctionDate,
+
+        @NotNull(message = "Starting price cannot be empty.")
+        @Min(value = 0, message = "Starting price must not be less than or equal to zero.")
+        Double price,
+
         @NotNull(message = "Auction end date cannot be empty.")
         LocalDateTime auctionDateEnd,
 
         Boolean isBuyNow,
-
-        Double buyNowPrice,
-
-        @NotNull(message = "Auction activity cannot be empty.")
-        String statusAuction,
-
-        @NotNull(message = "Auction buy now status cannot be empty.")
-        Boolean isBuyNowCompleted
+        @NotNull(groups = ItemValidationGroups.BuyNowActive.class, message = "Buy now price cannot be empty if buy now is active.")
+        Double buyNowPrice
 ) {
+        // Klasy walidacyjne dla grupy BuyNowActive
 }
