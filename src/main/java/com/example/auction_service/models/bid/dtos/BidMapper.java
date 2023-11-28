0c0a5dbccc83a4348d1c0ca182392ec821355e5a
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-28 21:07:12 +0100

    Builder added, BidMapper created

---
+++ src/main/java/com/example/auction_service/models/bid/dtos/BidMapper.java
@@ -0,0 +1,20 @@
+package com.example.auction_service.models.bid.dtos;
+
+import com.example.auction_service.models.address.Address;
+import com.example.auction_service.models.address.dtos.AddressResponseDTO;
+import com.example.auction_service.models.bid.Bid;
+
+public class BidMapper {
+
+    public static BidResponseDTO mapToBidResponseDTO(Bid bid) {
+        return BidResponseDTO.builder()
+                .bidId(bid.getId())
+                .customerId(bid.getCustomer().getId())
+                .customerEmail(bid.getCustomer().getEmail())
+                .auctionId(bid.getAuction().getId())
+                .bidValue(bid.getBidValue())
+                .bidStatus(String.valueOf(bid.getBidStatus()))
+                .isWinning(bid.getIsWinning())
+                .build();
+    }
+}
--- src/main/java/com/example/auction_service/models/bid/dtos/BidResponseDTO.java
+++ src/main/java/com/example/auction_service/models/bid/dtos/BidResponseDTO.java
@@ -1,11 +1,17 @@
 package com.example.auction_service.models.bid.dtos;
 
+import lombok.Builder;
+
 public record BidResponseDTO(
         Long bidId,
         Long customerId,
-        String customerEmail, // Opcjonalnie, dla lepszej czytelności
+        String customerEmail,
         Long auctionId,
         Double bidValue,
         String bidStatus,
-        Boolean isWinning
-) {}
+        Boolean isWinning) {
+
+    @Builder
+    public BidResponseDTO {}
+
+}
