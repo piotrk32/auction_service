7d04e2ef6b59ec8c4ad3b562f9ba1ad5b54b1fdf
piotrk32 <piotrkepistyuwm@gmail.com>
2023-11-28 12:45:40 +0100

    Status DONE changed to FINISHED

--- src/main/java/com/example/auction_service/models/auction/enums/StatusAuction.java
+++ src/main/java/com/example/auction_service/models/auction/enums/StatusAuction.java
@@ -1,5 +1,5 @@
 package com.example.auction_service.models.auction.enums;
 
 public enum StatusAuction {
-    NOT_STARTED, ACTIVE, CANCELED, DONE, DELETED
+    NOT_STARTED, ACTIVE, CANCELED, FINISHED, DELETED
 }
